a284d5bc6eefbcb7b020c2166e8e4ee70c8b3e04
h0wang <73479273+h0wang@users.noreply.github.com>
2020-11-20 22:40:40 +0000

    Add support to subscribe using ticker

--- meteor/subscription/__init__.py
+++ meteor/subscription/__init__.py
@@ -1,6 +1,12 @@
 from .impl import (BitstampTradeSubscription, CoinbaseTradeSubscription,
                    KrakenTradeSubscription)
 
+BSTP = BitstampTradeSubscription
+COIN = CoinbaseTradeSubscription
+KRKN = KrakenTradeSubscription
+
+
 __all__ = ["BitstampTradeSubscription",
            "CoinbaseTradeSubscription",
-           "KrakenTradeSubscription"]
+           "KrakenTradeSubscription",
+           "BSTP", "COIN", "KRKN"]
